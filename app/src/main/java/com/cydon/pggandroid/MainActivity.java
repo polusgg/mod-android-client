5f4a2464b064b38d9e05287d432255bcfb497903
cybershard <ashwatdd01@gmail.com>
2021-02-22 22:33:55 -0500

    Initial Commit - squash 01

---
+++ app/src/main/java/com/cydon/pggandroid/MainActivity.java
@@ -0,0 +1,45 @@
+package com.cydon.pggandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int START_UNITY_PLAYER_INTENT = 392;
+    private boolean isUnityLoaded = false;
+
+    static {
+        System.loadLibrary("pggandroidnative");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.launch_button).setOnClickListener((View v) -> {
+            isUnityLoaded = true;
+            initMod();
+            startActivityForResult(
+                    new Intent(this, UnityPlayerActivity.class)
+                            .setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT),
+                    START_UNITY_PLAYER_INTENT);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == START_UNITY_PLAYER_INTENT)
+            isUnityLoaded = false;
+    }
+
+    public native void initMod();
+}
